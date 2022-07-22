8b02e30fd725896f84ec6fd03bb8672f8c36688b
Jamee Kim <jameekim@nodaplife.me>
2022-07-21 22:23:17 -0400

    Async loading and transition of maps

---
+++ Source/RememberingWallace/Private/GameInstanceBase.cpp
@@ -0,0 +1,122 @@
+﻿// Elite Centennial
+
+#include "GameInstanceBase.h"
+
+#include "RememberingWallace.h"
+#include "Blueprint/UserWidget.h"
+#include "Kismet/GameplayStatics.h"
+#include "UObject/UObjectGlobals.h"
+
+UGameInstanceBase::UGameInstanceBase(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}
+
+void UGameInstanceBase::ChangeMap(const TSoftObjectPtr<UWorld> NewMap)
+{
+	DestinationMap = NewMap;
+	OnChangeLevelRequested();
+}
+
+void UGameInstanceBase::Init()
+{
+	Super::Init();
+
+	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &UGameInstanceBase::HandleMapLoadFinished);
+}
+
+void UGameInstanceBase::Shutdown()
+{
+	RemoveLoadingScreenFromViewport();
+
+	FCoreUObjectDelegates::PostLoadMapWithWorld.RemoveAll(this);
+
+	Super::Shutdown();
+}
+
+void UGameInstanceBase::StartLoadingMapAsset()
+{
+	LoadPackageAsync(
+		DestinationMap.GetLongPackageName(),
+		FLoadPackageAsyncDelegate::CreateUObject(this, &UGameInstanceBase::HandleAsyncPackageLoadFinished),
+		0,
+		PKG_ContainsMap);
+}
+
+UUserWidget* UGameInstanceBase::ShowLoadingScreen(const TSubclassOf<UUserWidget> LoadingScreenClass, const int32 ZOrder)
+{
+	if (bLoadingScreenShown)
+	{
+		return CurrentLoadingScreen.Get();
+	}
+
+	if (UUserWidget* Widget = UUserWidget::CreateWidgetInstance(*this, LoadingScreenClass, EName::None))
+	{
+		bLoadingScreenShown = true;
+		CurrentLoadingScreen = Widget;
+		GetGameViewportClient()->AddViewportWidgetContent(Widget->TakeWidget(), ZOrder);
+		return Widget;
+	}
+
+	return nullptr;
+}
+
+void UGameInstanceBase::HideLoadingScreen()
+{
+	if (!bLoadingScreenShown)
+	{
+		return;
+	}
+
+	RemoveLoadingScreenFromViewport();
+	bLoadingScreenShown = false;
+}
+
+void UGameInstanceBase::RemoveLoadingScreenFromViewport()
+{
+	// If the pointer to the widget is null, do nothing
+	if (!CurrentLoadingScreen)
+	{
+		return;
+	}
+
+	// Get the underlying low-level widget, hopefully the same one created from TakeWidget method in ShowLoadingScreen
+	const TSharedPtr<SWidget> Widget = CurrentLoadingScreen->GetCachedWidget();
+
+	if (Widget.IsValid())
+	{
+		GetGameViewportClient()->RemoveViewportWidgetContent(Widget.ToSharedRef());
+	}
+
+	// Remove the reference to the widget so that it can be garbage collected
+	CurrentLoadingScreen = nullptr;
+}
+
+void UGameInstanceBase::HandleAsyncPackageLoadFinished(
+	const FName& PackageName, UPackage* LoadedPackage, const EAsyncLoadingResult::Type Result)
+{
+	if (Result != EAsyncLoadingResult::Succeeded)
+	{
+		// TODO: error case handling
+		UE_LOG(LogWallace, Error, TEXT("Failed to async load package [%s]"), *PackageName.ToString());
+		return;
+	}
+
+	const UWorld* LoadedWorld = UWorld::FindWorldInPackage(LoadedPackage);
+	check(LoadedWorld && DestinationMap == LoadedWorld);
+	LoadedMapPackage = LoadedPackage;
+	UGameplayStatics::OpenLevelBySoftObjectPtr(this, DestinationMap);
+}
+
+void UGameInstanceBase::HandleMapLoadFinished(UWorld* LoadedWorld)
+{
+	// Clean up the reference to the package if the level is loaded into the game
+	if (LoadedMapPackage.Get())
+	{
+		if (LoadedWorld->GetName() == UWorld::FindWorldInPackage(LoadedMapPackage)->GetName())
+		{
+			OnMapTransitionFinished();
+			LoadedMapPackage = nullptr;
+			DestinationMap.Reset();
+		}
+	}
+}
---
+++ Source/RememberingWallace/Public/GameInstanceBase.h
@@ -0,0 +1,111 @@
+﻿// Elite Centennial
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+
+#include "GameInstanceBase.generated.h"
+
+/**
+ * Base class for the game instance class for this project
+ */
+UCLASS()
+class REMEMBERINGWALLACE_API UGameInstanceBase : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * Constructor
+	 */
+	UGameInstanceBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	/**
+	 * Whether the loading screen is currently shown on screen
+	 */
+	bool IsLoadingScreenShown() const { return bLoadingScreenShown; }
+
+	/**
+	 * Start the process of unloading the previous map and loading the requested map
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Map")
+	void ChangeMap(TSoftObjectPtr<UWorld> NewMap);
+
+	// BEGIN UGameInstance interface
+	virtual void Init() override;
+	virtual void Shutdown() override;
+	// END UGameInstance interface
+
+protected:
+	/**
+	 * The map used for transition between maps
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Map")
+	TSoftObjectPtr<UWorld> TransitionMap;
+
+	/**
+	 * Cached soft pointer to the map to transition to
+	 */
+	UPROPERTY(BlueprintReadOnly, Category = "Map")
+	TSoftObjectPtr<UWorld> DestinationMap;
+
+	/**
+	 * Whether the loading screen is currently shown on screen
+	 */
+	UPROPERTY(BlueprintReadOnly, Category = "LoadingScreen")
+	bool bLoadingScreenShown;
+
+	/**
+	 * Reference to the currently shown loading screen widget
+	 */
+	UPROPERTY(BlueprintReadOnly, Category = "LoadingScreen")
+	TObjectPtr<UUserWidget> CurrentLoadingScreen;
+
+	/**
+	 * Blueprint event called when ChangeLevel is called
+	 */
+	UFUNCTION(BlueprintImplementableEvent, Category = "Map")
+	void OnChangeLevelRequested();
+
+	/**
+	 * Blueprint event called when the transition to the requested map is done
+	 */
+	UFUNCTION(BlueprintImplementableEvent, Category = "Map")
+	void OnMapTransitionFinished();
+
+	/**
+	 * Start asynchronously loading the requested map asset into the memory
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Map")
+	void StartLoadingMapAsset();
+
+	/**
+	 * Create a new loading screen and show it on screen
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintCosmetic, Category = "LoadingScreen",
+		meta = (DeterminesOutputType = "LoadingScreenClass"))
+	UUserWidget* ShowLoadingScreen(TSubclassOf<UUserWidget> LoadingScreenClass, int32 ZOrder = 0);
+
+	/**
+	 * Destroy the currently shown loading screen if it exists
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintCosmetic, Category = "LoadingScreen")
+	void HideLoadingScreen();
+
+private:
+	// Remove the loading screen from the viewport and reset the reference to the widget
+	void RemoveLoadingScreenFromViewport();
+
+	// Handler method of the delegate for LoadPackageAsync
+	void HandleAsyncPackageLoadFinished(
+		const FName& PackageName, UPackage* LoadedPackage, EAsyncLoadingResult::Type Result);
+
+	// The loaded package for the map (prevents garbage collection)
+	UPROPERTY()
+	TObjectPtr<UPackage> LoadedMapPackage;
+
+	// Handler method of FCoreUObjectDelegates::PostLoadMapWithWorld
+	UFUNCTION()
+	void HandleMapLoadFinished(UWorld* LoadedWorld);
+};
